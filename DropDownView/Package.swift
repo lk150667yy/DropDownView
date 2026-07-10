64af7a5c15a1cf77aaafe9b842ce0616ccfb6fdd
lk-1989yy <lk_1989yy@163.com>
2026-07-10 11:43:11 +0800

    提交

---
+++ DropDownView/Package.swift
@@ -0,0 +1,34 @@
+//
+//  Package.swift
+//  DropDownView
+//
+//  Created by 刘凯 on 2026/7/10.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "DropDownView",
+    platforms: [
+        .iOS(.v14),    // 最低支持系统
+        .macOS(.v13)
+    ],
+    products: [
+        // 对外暴露Framework产品，别人import用MySDK
+        .library(
+            name: "DropDownView",
+            targets: ["DropDownView"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DropDownView",
+            sources: ["Sources/DropDownView/"],
+            // 资源文件在这里配置
+            resources: [
+                .process("Resources/")
+            ]
+        ),
+    ]
+)
+
