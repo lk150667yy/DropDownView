3bbd8100ce38cee9bf03249fe6c3985468fd791a
lk-1989yy <lk_1989yy@163.com>
2026-07-11 11:24:02 +0800

    提交新项目

---
+++ DropDownView/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DropDownView",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DropDownView",
+            targets: ["DropDownView"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
+    ], targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DropDownView"
+        ),
+
+
+    ],
+    swiftLanguageModes: [.v6]
+
+)
