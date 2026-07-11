dfc65b7c5714c66f3a94da75d5d07851dffed9df
lk-1989yy <lk_1989yy@163.com>
2026-07-11 14:27:42 +0800

    最新代码

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DropDownView",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -21,7 +18,10 @@ let package = Package(
         .target(
             name: "DropDownView"
         ),
-
+        .testTarget(
+            name: "DropDownViewTests",
+            dependencies: ["DropDownView"]
+        ),
     ],
-    swiftLanguageModes: [.v5]
+    swiftLanguageModes: [.v6]
 )
